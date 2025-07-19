3d504a744a6885cbff750e406c0581f587621767
fabiboz <fabiboz@alice.it>
2025-07-19 11:06:20 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,22 @@
+
+const CACHE_NAME = "traino-spinta-cache-v1";
+const urlsToCache = [
+  "./traino_spinta_checklist.html",
+  "./manifest.json",
+  "./icon-192.png",
+  "./icon-512.png"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
